50eff5038a62d113d1423066ce1bd09a01840f9b
RonyKositsky <rontkositsky@gmail.com>
2021-12-20 18:00:19 +0200

    New tables version

--- SRC/API.py
+++ SRC/API.py
@@ -1,5 +1,57 @@
-from tmdbv3api import Movie
+from tmdbv3api import TMDb
+from tmdbv3api import Discover
+from tmdbv3api import Genre
+from SRC.ModifiedConnector import ModifiedConnector
 
-movie = Movie()
+connector = ModifiedConnector()
 
-print("a")
+# Open connection to API.
+tmdb = TMDb()
+tmdb.api_key = '58a785782ea501b2d0684e158f09ad4b'
+tmdb.language = 'en'
+tmdb.debug = True
+
+# Get all the API movies.
+discover = Discover()
+genre = Genre()
+movies = discover.discover_movies({})
+
+
+def import_to_tables(dict):
+    cmds = [
+        "INSERT INTO movies (movie_id, title) VALUES (%s, %s)",
+        "INSERT INTO movies_countries (movie_id, language) VALUES (%s, %s)",
+        "INSERT INTO movies_votes (movie_id, avg_votes, votes) VALUES (%s, %s, %s)",
+        "INSERT INTO movies_genres (movie_id, genres) VALUES (%s, %s)"
+    ]
+    datas = [
+        (dict["id"], dict["title"]),
+        (dict["id"], dict["language"]),
+        (dict["id"], dict["vote_avg"], dict["votes"]),
+        (dict["id"], dict["genre"])
+    ]
+
+    for i in range(len(cmds)):
+        try:
+            connector.execute_with_params(cmds[i], datas[i])
+        except Exception as e:
+            print(f"{type(e).__name__} at line {e.__traceback__.tb_lineno} of {__file__}: {e}")
+
+
+def parse_movie(movie_obj):
+    params_dict = {
+        "id": movie_obj["id"],
+        "title": movie_obj["title"],
+        "language": movie_obj["original_language"],
+        "vote_avg": movie_obj["vote_average"],
+        "votes": movie_obj["votes"]}
+    genre_str = ""
+    for g in movie_obj["genres"]:
+        genre_str += genre[g]
+    params_dict["genre"] = genre_str
+
+    import_to_tables(params_dict)
+
+
+for movie in movies:
+    parse_movie(movie)
--- SRC/Connection.py
+++ SRC/Connection.py
@@ -1,8 +1,14 @@
 import csv
 from ModifiedConnector import ModifiedConnector
-from datetime import datetime
 
 connector = ModifiedConnector()
+
+
+def create_table(cmd):
+    try:
+        connector.execute(cmd)
+    except Exception as e:
+        print(f"{type(e).__name__} at line {e.__traceback__.tb_lineno} of {__file__}: {e}")
 
 
 def create_db_tables():
@@ -10,268 +16,189 @@ def create_db_tables():
         Creating the tables in our database.
         First, it will delete any possible existing table, then it will create them.
     """
-    # remove all existing tables
-    table_names = ["movies", "movies_votes", "actors", "ratings"]
-    for name in table_names:
-        cmd = f'''DROP TABLE {name};'''
-        try:
-            connector.execute(cmd)
-        except Exception as e:
-            print(f"{type(e).__name__} at line {e.__traceback__.tb_lineno} of {__file__}: {e}")
+    cmds = ['CREATE TABLE movies (movie_id INT, title VARCHAR(1000), year INT);',
+            'CREATE TABLE movies_genres (movie_id INT, genres VARCHAR(1000));',
+            'CREATE TABLE movies_countries (movie_id INT, country VARCHAR(1000), language VARCHAR(1000));',
+            'CREATE TABLE movies_productions (movie_id INT, director VARCHAR(1000), writer VARCHAR(1000), productions '
+            'VARCHAR(1000), actors VARCHAR(1000), description VARCHAR(1000));',
+            'CREATE TABLE movies_votes (movie_id INT, avg_votes FLOAT, votes INT, critics_votes INT);',
+            'CREATE TABLE movies_budget (movie_id INT, budget FLOAT);',
+            'CREATE TABLE actors (actor_id INT, name VARCHAR(1000), bio VARCHAR(10000));',
+            'CREATE TABLE actors_birth (actor_id INT, place_of_birth VARCHAR(1000));',
+            'CREATE TABLE ratings (movie_id INT, male_avg FLOAT, female_avg FLOAT);']
 
-    cmd = '''CREATE TABLE movies (movie_id INT, title VARCHAR(1000), year INT, genre VARCHAR(1000), duration INT, 
-            country VARCHAR(1000), language VARCHAR(1000), production VARCHAR(30), 
-            actors VARCHAR(1000), description VARCHAR(1000)); '''
-    try:
-        connector.execute(cmd)
-    except Exception as e:
-        print(f"{type(e).__name__} at line {e.__traceback__.tb_lineno} of {__file__}: {e}")
-
-    cmd = '''CREATE TABLE movies_votes (movie_id INT, avg_vote FLOAT, votes INT, budget INT ); '''
-    try:
-        connector.execute(cmd)
-    except Exception as e:
-        print(f"{type(e).__name__} at line {e.__traceback__.tb_lineno} of {__file__}: {e}")
-
-    cmd = '''CREATE TABLE actors (actor_id INT, name VARCHAR(100), birth_name VARCHAR(100), height INT, 
-            bio VARCHAR(16383), birth_date DATE, death_date DATE, divorces INT, children INT); '''
-    try:
-        connector.execute(cmd)
-    except Exception as e:
-        print(f"{type(e).__name__} at line {e.__traceback__.tb_lineno} of {__file__}: {e}")
-
-    cmd = '''CREATE TABLE ratings (movie_id INT, weighted_average_vote FLOAT, mean_vote FLOAT, median_vote INT, 
-    allgenders_0age_avg_vote FLOAT, allgenders_0age_votes INT, allgenders_18age_avg_vote FLOAT, 
-    allgenders_18age_votes INT, allgenders_30age_avg_vote FLOAT, allgenders_30age_votes INT, 
-    allgenders_45age_avg_vote FLOAT, allgenders_45age_votes INT, males_allages_avg_vote FLOAT, males_allages_votes 
-    INT, males_0age_avg_vote FLOAT, males_0age_votes INT, males_18age_avg_vote FLOAT, males_18age_votes INT, 
-    males_30age_avg_vote FLOAT, males_30age_votes INT, males_45age_avg_vote FLOAT, males_45age_votes INT, 
-    females_allages_avg_vote FLOAT, females_allages_votes INT, females_0age_avg_vote FLOAT, females_0age_votes INT, 
-    females_18age_avg_vote FLOAT, females_18age_votes INT, females_30age_avg_vote FLOAT, females_30age_votes INT, 
-    females_45age_avg_vote FLOAT, females_45age_votes INT, top1000_voters_rating FLOAT, top1000_voters_votes INT, 
-    us_voters_rating FLOAT); '''
-    try:
-        connector.execute(cmd)
-    except Exception as e:
-        print(f"{type(e).__name__} at line {e.__traceback__.tb_lineno} of {__file__}: {e}")
+    for cmd in cmds:
+        create_table(cmd)
 
 
-def insert_movies_votes_data():
-    in_f = open("IMDb movies.csv", encoding="cp437", mode="r")
-    csv_reader = csv.reader(in_f, delimiter=',')
-    cmd = "INSERT INTO movies_votes (movie_id, avg_vote, votes, budget) VALUES (%s, %s, %s, %s)"
-    skip = True
-    i = 0
-    for row in csv_reader:
-        if skip:  # skip the title row.
-            skip = False
-        else:
-            i += 1
-            try:
-                movie_id = int(row[0][2:])
-                avg_vote = float(row[14])
-                votes = int(row[15])
-                budget_str = row[16]
-                budget = 0
-                if "$" in budget_str:
-                    budget = ''.join((ch if ch in '0123456789' else '') for ch in budget_str)
-                data = (movie_id, avg_vote, votes, budget)
-                connector.execute_with_params(cmd, data)
-                print(i)
-            except Exception as e:
-                print(row)
-                print(f"{type(e).__name__} at line {e.__traceback__.tb_lineno} of {__file__}: {e}")
+def open_csv_reader(csv_file_name):
+    in_f = open(f"{csv_file_name}", encoding="cp437", mode="r")
+    return csv.reader(in_f, delimiter=',')
 
 
 def insert_movies_data():
-    in_f = open("IMDb movies.csv", encoding="cp437", mode="r")
-    csv_reader = csv.reader(in_f, delimiter=',')
-    cmd = "INSERT INTO movies_votes (movie_id, avg_vote, votes, budget) VALUES (%s, %s, %s, %s)"
-    skip = True
-    i = 0
-    for row in csv_reader:
-        if skip:  # skip the title row.
-            skip = False
-        else:
-            i += 1
-            try:
-                movie_id = int(row[0][2:])
-                avg_vote = float(row[14])
-                votes = int(row[15])
-                budget_str = row[16]
-                budget = 0
-                if "$" in budget_str:
-                    budget = ''.join((ch if ch in '0123456789' else '') for ch in budget_str)
-                data = (movie_id, avg_vote, votes, budget)
-                connector.execute_with_params(cmd, data)
-                print(i)
-            except Exception as e:
-                print(row)
-                print(f"{type(e).__name__} at line {e.__traceback__.tb_lineno} of {__file__}: {e}")
+    csv_reader = open_csv_reader("IMDb movies.csv")
+    cmd = "INSERT INTO movies (movie_id, title, year) VALUES (%s, %s, %s)"
 
-
-def parse_birth_date(date_str):
-    if len(date_str) == 0:
-        return None
-    if "c." in date_str:
-        date_str = date_str.replace("c.", "").strip()
-    if "BC" in date_str:
-        date_str = date_str.replace("BC", "").strip()
-    try:
-        return datetime.fromisoformat(date_str)
-    except:
+    for row in list(csv_reader)[1:]:
         try:
-            return datetime.strptime(date_str, '%B %d, %Y')
-        except:
-            try:
-                return datetime.strptime(date_str, '%B, %Y')
-            except:
-                try:
-                    return datetime.strptime(date_str, '%B %d')
-                except:
-                    return datetime.strptime(date_str, '%Y')
+            movie_id = int(row[0][2:])
+            title = row[1]
+            year = int(''.join((ch if ch in '0123456789' else '') for ch in row[3]))
+            data = (movie_id, title, year)
+            connector.execute_with_params(cmd, data)
+        except Exception as e:
+            print(f"{type(e).__name__} at line {e.__traceback__.tb_lineno} of {__file__}: {e}")
+    print("Finished")
 
 
-def parse_death_date(date_str):
-    if len(date_str) == 0:
-        return None
-    date = date_str
-    if "in" in date_str:
-        date = date_str.split("in")[0].strip()
-    return parse_birth_date(date)
+def insert_movies_genres_data():
+    csv_reader = open_csv_reader("IMDb movies.csv")
+    cmd = "INSERT INTO movies_genres (movie_id, genres) VALUES (%s, %s)"
+    for row in list(csv_reader)[1:]:
+        try:
+            movie_id = int(row[0][2:])
+            genre = row[5]
+            data = (movie_id, genre)
+            connector.execute_with_params(cmd, data)
+        except Exception as e:
+            print(f"{type(e).__name__} at line {e.__traceback__.tb_lineno} of {__file__}: {e}")
+    print("Finished")
+
+
+def insert_movies_countries_data():
+    csv_reader = open_csv_reader("IMDb movies.csv")
+    cmd = "INSERT INTO movies_countries (movie_id, country, language) VALUES (%s, %s, %s)"
+    for row in list(csv_reader)[1:]:
+        try:
+            movie_id = int(row[0][2:])
+            country = row[7]
+            language = row[8]
+            if language.strip() == "None" or language.strip() == "":
+                language = None
+            data = (movie_id, country, language)
+            connector.execute_with_params(cmd, data)
+        except Exception as e:
+            print(f"{type(e).__name__} at line {e.__traceback__.tb_lineno} of {__file__}: {e}")
+    print("Finished")
+
+
+def insert_movies_productions_data():
+    csv_reader = open_csv_reader("IMDb movies.csv")
+    cmd = "INSERT INTO movies_productions (movie_id, director, writer, productions, actors, description)" \
+          " VALUES (%s, %s, %s, %s, %s, %s)"
+    for row in list(csv_reader)[1:]:
+        try:
+            movie_id = int(row[0][2:])
+            director = row[9]
+            writer = row[10]
+            productions = row[11]
+            actors = row[12]
+            description = row[13]
+            data = (movie_id, director, writer, productions, actors, description)
+            connector.execute_with_params(cmd, data)
+
+        except Exception as e:
+            print(f"{type(e).__name__} at line {e.__traceback__.tb_lineno} of {__file__}: {e}")
+    print("Finished")
+
+
+def insert_movies_votes_data():
+    csv_reader = open_csv_reader("IMDb movies.csv")
+    cmd = "INSERT INTO movies_votes (movie_id, avg_votes, votes, critics_votes) VALUES (%s, %s, %s, %s)"
+
+    for row in list(csv_reader)[1:]:
+        try:
+            movie_id = int(row[0][2:])
+            avg_votes = float(row[14])
+            votes = int(row[15])
+            critics_votes = 0
+            if row[21] != "":
+                critics_votes = float(row[21])
+            data = (movie_id, avg_votes, votes, critics_votes)
+            connector.execute_with_params(cmd, data)
+        except Exception as e:
+            print(f"{type(e).__name__} at line {e.__traceback__.tb_lineno} of {__file__}: {e}")
+    print("Finished")
+
+
+def insert_movies_budget_data():
+    csv_reader = open_csv_reader("IMDb movies.csv")
+    cmd = "INSERT INTO movies_budget (movie_id, budget) VALUES (%s, %s)"
+    i = 0
+    counter = 0
+    for row in list(csv_reader)[1:]:
+        try:
+            movie_id = int(row[0][2:])
+            budget_str = row[16]
+            budget = 0
+            if "$" in budget_str:
+                budget = float(''.join((ch if ch in '0123456789' else '') for ch in budget_str))
+            data = (movie_id, budget)
+            connector.execute_with_params(cmd, data)
+            if i % 5000 == 0:
+                i = 0
+                print("dot " + str(counter))
+                counter += 1
+            i += 1
+        except Exception as e:
+            print(f"{type(e).__name__} at line {e.__traceback__.tb_lineno} of {__file__}: {e}")
+    print("Finished")
 
 
 def insert_actors_data():
-    in_f = open("IMDb names.csv", encoding="cp437", mode="r")
-    csv_reader = csv.reader(in_f, delimiter=',')
-
-    cmd = "INSERT INTO actors (actor_id, name, birth_name, height, bio, birth_date, death_date, divorces, children) " \
-          "VALUES (%s, %s, %s, %s, %s, %s, %s, %s, %s) "
-    max_len = 16000
-    skip = True
-    i = 0
-    for row in csv_reader:
-        if skip:  # skip the title row.
-            skip = False
-        else:
-            i += 1
-            try:
-                actor_id = int(row[0][2:])
-                name = row[1]
-                birth_name = row[2]
-                height = 0
-                if not (len(row[3]) == 0):
-                    height = int(row[3])
-                bio = row[4]
-                bio = bio[:min(len(bio), max_len)]
-                birth_date = parse_birth_date(row[5].split("in")[0].strip())
-                death_date = parse_death_date(row[6])
-                divorces = int(row[14])
-                children = int(row[16])
-                data = (actor_id, name, birth_name, height, bio, birth_date, death_date, divorces, children)
-                connector.execute_with_params(cmd, data)
-                if i % 5000 == 0:
-                    print(i)
-            except Exception as e:
-                print(row)
-                print(f"{type(e).__name__} at line {e.__traceback__.tb_lineno} of {__file__}: {e}")
+    csv_reader = open_csv_reader("IMDb names.csv")
+    cmd = "INSERT INTO actors (actor_id, name, bio) VALUES (%s, %s, %s)"
+    for row in list(csv_reader)[1:]:
+        try:
+            actor_id = int(row[0][2:])
+            name = row[1]
+            bio = row[4]
+            data = (actor_id, name, bio)
+            connector.execute_with_params(cmd, data)
+        except Exception as e:
+            print(f"{type(e).__name__} at line {e.__traceback__.tb_lineno} of {__file__}: {e}")
+    print("Finished")
 
 
 def insert_ratings_data():
-    in_f = open("IMDb ratings.csv", encoding="cp437", mode="r")
-    csv_reader = csv.reader(in_f, delimiter=',')
-
-    cmd = "INSERT INTO ratings (movie_id, weighted_average_vote, mean_vote, median_vote, " \
-          "allgenders_0age_avg_vote, allgenders_0age_votes, allgenders_18age_avg_vote," \
-          "allgenders_18age_votes ,allgenders_30age_avg_vote ,allgenders_30age_votes ," \
-          "allgenders_45age_avg_vote ,allgenders_45age_votes ,males_allages_avg_vote ,males_allages_votes, " \
-          "males_0age_avg_vote ,males_0age_votes ,males_18age_avg_vote ,males_18age_votes , " \
-          "males_30age_avg_vote ,males_30age_votes ,males_45age_avg_vote ,males_45age_votes , " \
-          "females_allages_avg_vote ,females_allages_votes ,females_0age_avg_vote ,females_0age_votes ," \
-          "females_18age_avg_vote ,females_18age_votes ,females_30age_avg_vote ,females_30age_votes," \
-          "females_45age_avg_vote ,females_45age_votes , top1000_voters_rating ,top1000_voters_votes," \
-          "us_voters_rating) " \
-          "VALUES (%s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, " \
-          "%s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s);"
-    skip = True
-    for mrow in csv_reader:
-        if skip:  # skip the title row.
-            skip = False
-        else:
-            movie_id = 0
-            try:
-                row = []
-                for num in mrow:
-                    x = num
-                    if len(num) == 0:
-                        x = "0"
-                    row.append(x)
-
-                movie_id = int(row[0][2:])
-                weighted_average_vote = float(row[1])
-                mean_vote = float(row[3])
-                median_vote = float(row[4])
-                allgenders_0age_avg_vote = float(row[15])
-                allgenders_0age_votes = int(float(row[16]))
-                allgenders_18age_avg_vote = float(row[17])
-                allgenders_18age_votes = int(float(row[18]))
-                allgenders_30age_avg_vote = float(row[19])
-                allgenders_30age_votes = int(float(row[20]))
-                allgenders_45age_avg_vote = float(row[21])
-                allgenders_45age_votes = int(float(row[22]))
-                males_allages_avg_vote = float(row[23])
-                males_allages_votes = int(float(row[24]))
-                males_0age_avg_vote = float(row[25])
-                males_0age_votes = int(float(row[26]))
-                males_18age_avg_vote = float(row[27])
-                males_18age_votes = int(float(row[28]))
-                males_30age_avg_vote = float(row[29])
-                males_30age_votes = int(float(row[30]))
-                males_45age_avg_vote = float(row[31])
-                males_45age_votes = int(float(row[32]))
-                females_allages_avg_vote = float(row[33])
-                females_allages_votes = int(float(row[34]))
-                females_0age_avg_vote = float(row[35])
-                females_0age_votes = int(float(row[36]))
-                females_18age_avg_vote = float(row[37])
-                females_18age_votes = int(float(row[38]))
-                females_30age_avg_vote = float(row[39])
-                females_30age_votes = int(float(row[40]))
-                females_45age_avg_vote = float(row[41])
-                females_45age_votes = int(float(row[42]))
-                top1000_voters_rating = float(row[43])
-                top1000_voters_votes = int(float(row[44]))
-                us_voters_rating = float(row[45])
-                data = (movie_id, weighted_average_vote, mean_vote, median_vote, allgenders_0age_avg_vote, allgenders_0age_votes,
-                        allgenders_18age_avg_vote, allgenders_18age_votes, allgenders_30age_avg_vote,
-                        allgenders_30age_votes, allgenders_45age_avg_vote, allgenders_45age_votes,
-                        males_allages_avg_vote, males_allages_votes,  males_0age_avg_vote,
-                        males_0age_votes, males_18age_avg_vote, males_18age_votes, males_30age_avg_vote,
-                        males_30age_votes, males_45age_avg_vote, males_45age_votes, females_allages_avg_vote,
-                        females_allages_votes, females_0age_avg_vote, females_0age_votes, females_18age_avg_vote,
-                        females_18age_votes, females_30age_avg_vote, females_30age_votes, females_45age_avg_vote,
-                        females_45age_votes, top1000_voters_rating, top1000_voters_votes, us_voters_rating)
-                connector.execute_with_params(cmd, data)
-            except Exception as e:
-                print(movie_id)
-                print(f"{type(e).__name__} at line {e.__traceback__.tb_lineno} of {__file__}: {e}")
-
-
-def modifications():
-    cmd = '''ALTER TABLE actors MODIFY bio VARCHAR(16000);'''
-    connector.execute(cmd)
+    csv_reader = open_csv_reader("IMDb ratings.csv")
+    cmd = "INSERT INTO ratings (movie_id, male_avg, female_avg) VALUES (%s, %s, %s)"
+    for row in list(csv_reader)[1:]:
+        try:
+            movie_id = int(row[0][2:])
+            male_avg = float(row[23])
+            female_avg = float(row[33])
+            data = (movie_id, male_avg, female_avg)
+            connector.execute_with_params(cmd, data)
+        except Exception as e:
+            print(f"{type(e).__name__} at line {e.__traceback__.tb_lineno} of {__file__}: {e}")
+    print("Finished")
 
 
 def insert_all_data():
     insert_movies_data()
+    insert_movies_genres_data()
+    insert_movies_countries_data()
+    insert_movies_productions_data()
     insert_movies_votes_data()
+    insert_movies_budget_data()
     insert_actors_data()
     insert_ratings_data()
+
+
+def modifications():
+    cmd = '''ALTER TABLE movies_votes MODIFY critics_votes FLOAT;'''
+    connector.execute(cmd)
 
 
 if __name__ == "__main__":
     # create_db_tables()
     # insert_all_data()
-    insert_ratings_data()
+    # insert_movies_data()
     # modifications()
     # connector.close()
+    insert_movies_budget_data()
+    # insert_actors_data()
+    # insert_ratings_data()
+    connector.close()
